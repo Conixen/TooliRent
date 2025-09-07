0a2d40f69384db0b7629e1960077643641c668c7
Leon <leon.json57@gmail.com>
2025-09-07 21:22:18 +0200

    small adjustments

--- TooliRent/DTO;s/CategoryDTOs/CategoryDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TooliRent.DTO_s.CategoryDTOs
-{
-    public class CategoryDto    // admin snd user
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-        public string Description { get; set; } = string.Empty;
-    }
-}
