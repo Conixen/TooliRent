0cb717256f33a9b9edd86cae60e19bc76e89c794
Leon <leon.json57@gmail.com>
2025-09-10 22:10:01 +0200

    Vali ordeerdetails

---
+++ TooliRent/Validators/OrderDetailsValidators/CreateOrderDTOVali.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using TooliRent.DTO_s.OrderDetailsDTOs;
+namespace TooliRent.Validators.OrderDetailsValidators
+{
+    public class CreateOrderDTOVali : AbstractValidator<CreateOrderDTO>
+    {
+        public CreateOrderDTOVali() 
+        {
+            RuleFor(x => x.Date2Hire)
+                .NotEmpty();
+
+
+            RuleFor(x => x.Date2Return)
+                .NotEmpty();
+
+            RuleFor(x => x.ToolId)
+                .NotEmpty()
+                .GreaterThan(0);
+
+        }
+    }
+}
---
+++ TooliRent/Validators/OrderDetailsValidators/UpdateOrderDTOVali.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using TooliRent.DTO_s.OrderDetailsDTOs;
+namespace TooliRent.Validators.OrderDetailsValidators
+{
+    public class UpdateOrderDTOVali : AbstractValidator<UpdateOrderDTO>
+    {
+        public UpdateOrderDTOVali()
+        {
+            RuleFor(x => x.Date2Hire)
+                .NotEmpty();
+
+
+            RuleFor(x => x.Date2Return)
+                .NotEmpty();
+
+            RuleFor(x => x.ToolId)
+                .NotEmpty()
+                .GreaterThan(0);
+        }
+    }
+}
