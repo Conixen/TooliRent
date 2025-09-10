1490052a466b5ec1af7596df272ee0789e5b4fad
Leon <leon.json57@gmail.com>
2025-09-10 21:56:26 +0200

    Category Vali

---
+++ TooliRent/Validators/CategoryValidators/CreateCategoryDTOVali.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using TooliRent.DTO_s.CategoryDTOs;
+
+
+namespace TooliRent.Validators.CategoryValidators
+{
+    public class CreateCategoryDTOVali : AbstractValidator<CreateCategoryDTO>
+    {
+        public CreateCategoryDTOVali()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .MaximumLength(50);
+
+            RuleFor(x => x.Description)
+                .NotEmpty()
+                .MaximumLength(255);
+        }
+    }
+}
---
+++ TooliRent/Validators/CategoryValidators/UpdateCategoryDTOVali.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using TooliRent.DTO_s.CategoryDTOs;
+
+namespace TooliRent.Validators.CategoryValidators
+{
+    public class UpdateCategoryDTOVali : AbstractValidator<UpdateCategoryDTO>
+    {
+        public UpdateCategoryDTOVali()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .MaximumLength(50);
+
+            RuleFor(x => x.Description)
+                .NotEmpty()
+                .MaximumLength(255);
+        }
+    }
+}
