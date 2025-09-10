84e80903b70fb33fb8966b02600334b47aad6279
Leon <leon.json57@gmail.com>
2025-09-10 21:43:58 +0200

    Validators for auth and adjusted refrences

--- TooliRent.Services/DTOs/AuthDTOs/UpdateUserDTO.cs
+++ TooliRent.Services/DTOs/AuthDTOs/UpdateUserDTO.cs
@@ -12,7 +12,13 @@ namespace TooliRent.DTOs.AuthDTOs
         [MaxLength(75)]
         public string LastName { get; set; } = string.Empty;
 
-        [MaxLength(20)]
-        public string? PhoneNumber { get; set; }
+        [Required]
+        [EmailAddress]
+        [MaxLength(150)]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [MinLength(6)]
+        public string Password { get; set; } = string.Empty;
     }
 }
