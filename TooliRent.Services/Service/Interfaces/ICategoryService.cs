ee78f2ac83e6137b6b116425e8a81e38020e36ed
Leon <leon.json57@gmail.com>
2025-09-15 17:43:09 +0200

    category inter

---
+++ TooliRent.Services/Service/Interfaces/ICategoryService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TooliRent.DTO_s.CategoryDTOs;
+
+namespace TooliRent.Services.Service.Interfaces
+{
+    public interface ICategoryService
+    {
+        Task<CategoryDto> CreateCategoryAsync(CreateCategoryDTO dto);
+        Task<List<CategoryDto>> GetAllCategoriesAsync();
+        Task<CategoryDto> GetCategoryByIdAsync(int id);
+        Task UpdateCategoryAsync(int id, UpdateCategoryDTO dto);
+        Task DeleteCategoryAsync(int id);
+    }
+}
