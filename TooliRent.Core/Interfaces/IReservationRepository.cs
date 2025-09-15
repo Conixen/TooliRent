62b0bc849ba1c685063e108cd1bfd5963bcf0215
Leon <leon.json57@gmail.com>
2025-09-15 17:59:29 +0200

    Irepositorys

---
+++ TooliRent.Core/Interfaces/IReservationRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TooliRent.Models;
+
+namespace TooliRent.Core.Interfaces
+{
+    public interface IReservationRepository
+    {
+        Task<Reservation> GetByIdAsync(int id);
+        Task<List<Reservation>> GetAllAsync();
+        Task<Reservation> AddAsync(Reservation reservation);
+        Task UpdateAsync(Reservation reservation);
+        Task DeleteAsync(int id);
+    }
+}
---
+++ TooliRent.Core/Interfaces/IReservationToolRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TooliRent.Models;
+
+namespace TooliRent.Core.Interfaces
+{
+    public interface IReservationToolRepository
+    {
+        Task<ReservationTool> GetByIdAsync(int id);
+        Task<List<ReservationTool>> GetAllAsync();
+        Task<ReservationTool> AddAsync(ReservationTool reservationTool);
+        Task UpdateAsync(ReservationTool reservationTool);
+        Task DeleteAsync(int id);
+    }
+}
