1e5b7db52d4f591b94028f79217d2db605354229
Leon <leon.json57@gmail.com>
2025-09-16 10:16:04 +0200

    Removed dtos from service

--- TooliRent.Services/DTOs/AuthDTOs/AuthResponseDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TooliRent.DTOs.AuthDTOs
-{
-    public class AuthResponseDTO
-    {
-        public string Token { get; set; } = string.Empty;
-        public DateTime Expires { get; set; }
-        public UserDTO User { get; set; } = null!;
-    }
-}
