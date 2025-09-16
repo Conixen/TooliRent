e8f76595b762cd12b0990ed493dae6094c9f2a87
Leon <leon.json57@gmail.com>
2025-09-16 10:44:53 +0200

    Repositorys category

---
+++ TooliRent.Infrastructure/Repositories/CategoryRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TooliRent.Infrastructure.Data;
+using TooliRent.Models;
+using TooliRent.Core.Interfaces.IRepository;
+using Microsoft.EntityFrameworkCore;
+
+namespace TooliRent.Infrastructure.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly TooliRentContext _context;
+        
+        public async Task<Category> GetByIdAsync(int id)
+        {
+            return await _context.Categorys.FindAsync(id);
+        }
+        public async Task<List<Category>> GetAllAsync()
+        {
+            return await _context.Categorys.ToListAsync();
+        }
+        public async Task<Category> AddAsync(Category category)
+        {
+            _context.Categorys.Add(category);
+            await _context.SaveChangesAsync();
+            return category;
+        }
+        public async Task UpdateAsync(Category category)
+        {
+            _context.Categorys.Update(category);
+            await _context.SaveChangesAsync();
+        }
+        public async Task DeleteAsync(int id)
+        {
+            var category = await _context.Categorys.FindAsync(id);
+            if (category != null)
+            {
+                _context.Categorys.Remove(category);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
