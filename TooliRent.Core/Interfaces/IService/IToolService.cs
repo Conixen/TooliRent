1249a2eef1ba4e1e6227f4d5e87e31ab5f4fab42
Leon <leon.json57@gmail.com>
2025-09-24 16:31:05 +0200

    updated interface

--- TooliRent.Core/Interfaces/IService/IToolService.cs
+++ TooliRent.Core/Interfaces/IService/IToolService.cs
@@ -10,16 +10,14 @@ namespace TooliRent.Core.Interfaces.IService
 {
     public interface IToolService
     {
-        public interface IToolService
-        {
-            Task<ToolDetailDTO?> GetByIdAsync(int id);
-            Task<List<ToolSummaryDTO>> GetAllAsync();
-            Task<List<ToolSummaryDTO>> GetAvailableAsync();
-            Task<List<ToolSummaryDTO>> GetByCategoryAsync(int categoryId);
-            Task<ToolDetailDTO> CreateAsync(CreateToolDto dto);
-            Task UpdateAsync(int id, UpdateToolDTO dto);
-            Task UpdateStatusAsync(int id, UpdateToolStatusDTO dto);
-            Task DeleteAsync(int id);
-            Task<List<ToolSelectDTO>> GetForSelectAsync();
-        }
+        Task<ToolDetailDTO?> GetByIdAsync(int id);
+        Task<List<ToolSummaryDTO>> GetAllAsync();
+        Task<List<ToolSummaryDTO>> GetAvailableAsync();
+        Task<List<ToolSummaryDTO>> GetByCategoryAsync(int categoryId);
+        Task<ToolDetailDTO> CreateAsync(CreateToolDto dto);
+        Task UpdateAsync(int id, UpdateToolDTO dto);
+        Task UpdateStatusAsync(int id, UpdateToolStatusDTO dto);
+        Task DeleteAsync(int id);
+        Task<List<ToolSelectDTO>> GetForSelectAsync();
     }
+}
