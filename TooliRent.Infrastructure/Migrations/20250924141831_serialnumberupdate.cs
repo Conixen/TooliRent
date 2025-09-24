2afcc96aa36e9b05c7d6294514a97adad3858761
Leon <leon.json57@gmail.com>
2025-09-24 16:31:22 +0200

    service and migration serialnu,mber

---
+++ TooliRent.Infrastructure/Migrations/20250924141831_serialnumberupdate.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TooliRent.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class serialnumberupdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SerialNumber",
+                table: "Tools",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Tools",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "SerialNumber",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Tools",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "SerialNumber",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Tools",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "SerialNumber",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Tools",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "SerialNumber",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Tools",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "SerialNumber",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Tools",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "SerialNumber",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Tools",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "SerialNumber",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Tools",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "SerialNumber",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Tools",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "SerialNumber",
+                value: null);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SerialNumber",
+                table: "Tools");
+        }
+    }
+}
