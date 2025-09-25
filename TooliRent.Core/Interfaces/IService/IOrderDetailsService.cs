8c8ea023e721fabfbcdd145ccd780f5c6b8fcf01
Leon <leon.json57@gmail.com>
2025-09-25 20:03:56 +0200

    i service

--- TooliRent.Core/Interfaces/IService/IOrderDetailsService.cs
+++ TooliRent.Core/Interfaces/IService/IOrderDetailsService.cs
@@ -3,17 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TooliRent.DTO_s.OrderDetailsDTOs;
 using TooliRent.Models;
 
 namespace TooliRent.Core.Interfaces.IService
 {
     public interface IOrderDetailsService
     {
-        Task<OrderDeatils> GetByIdAsync(int id);
-        Task<List<OrderDeatils>> GetAllAsync();
-        Task<OrderDeatils> AddAsync(OrderDeatils orderDeatils);
-        Task UpdateAsync(OrderDeatils orderDeatils);
-        Task DeleteAsync(int id);
+        // Basic CRUD
+        Task<OrderSummaryDTO?> GetByIdAsync(int id);
+        Task<List<OrderSummaryDTO>> GetAllAsync();
+        Task<List<OrderSummaryDTO>> GetByUserIdAsync(int userId);
+        Task DeleteAsync(int id);   // admin only
+
+        // Create & Update
+        Task<OrderSummaryDTO> CreateAsync(CreateOrderDTO dto, int userId);
+        Task UpdateAsync(int id, UpdateOrderDTO dto);
+
+        // Order lifecycle
+        Task CheckOutAsync(int id);
+        Task ReturnAsync(int id);
+        Task CancelAsync(int id, OrderCancelDTO dto);
+
+        // Business logic
+        Task<List<OrderSummaryDTO>> GetActiveOrdersAsync();
+        Task<List<OrderSummaryDTO>> GetOverdueOrdersAsync();
+        Task<decimal> CalculateLateFeeAsync(int orderId);
+
     }
+}
 
 }
