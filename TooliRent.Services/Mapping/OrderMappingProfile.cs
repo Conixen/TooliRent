cb498715d3235d4463762c2ccc5e597fb8bfc349
Leon <leon.json57@gmail.com>
2025-09-25 20:04:04 +0200

    mapping uppdated

--- TooliRent.Services/Mapping/OrderMappingProfile.cs
+++ TooliRent.Services/Mapping/OrderMappingProfile.cs
@@ -7,50 +7,67 @@ namespace TooliRent.Mapping
     {
         public OrderMappingProfile()
         {
-            // CREATE: CreateOrderDto -> OrderDetails
+            // CREATE: CreateOrderDTO -> OrderDeatils
             CreateMap<CreateOrderDTO, OrderDeatils>()
-                .ForMember(dest => dest.Status, opt => opt.MapFrom(src => "Pending")) 
-                .ForMember(dest => dest.TotalPrice, opt => opt.Ignore()) 
-                .ForMember(dest => dest.LateFee, opt => opt.MapFrom(src => 0)) 
-                .ForMember(dest => dest.Date2Hire, opt => opt.Ignore()) 
-                .ForMember(dest => dest.Date2Return, opt => opt.Ignore()); 
+                .ForMember(dest => dest.Status, opt => opt.MapFrom(src => "Pending"))
+                .ForMember(dest => dest.TotalPrice, opt => opt.Ignore()) // Calculated in service
+                .ForMember(dest => dest.LateFee, opt => opt.MapFrom(src => 0))
+                .ForMember(dest => dest.Id, opt => opt.Ignore())
+                .ForMember(dest => dest.UserId, opt => opt.Ignore()) // Set in service
+                .ForMember(dest => dest.ReservationId, opt => opt.Ignore())
+                .ForMember(dest => dest.CheckedOutAt, opt => opt.Ignore())
+                .ForMember(dest => dest.ReturnedAt, opt => opt.Ignore())
+                .ForMember(dest => dest.CreatedAt, opt => opt.Ignore()) // Set in service
+                .ForMember(dest => dest.UpdatedAt, opt => opt.Ignore()) // Set in service
+                .ForMember(dest => dest.User, opt => opt.Ignore())
+                .ForMember(dest => dest.Tool, opt => opt.Ignore())
+                .ForMember(dest => dest.Reservation, opt => opt.Ignore());
 
-            // UPDATE: UpdateOrderDto -> OrderDetails
+            // UPDATE: UpdateOrderDTO -> OrderDeatils
             CreateMap<UpdateOrderDTO, OrderDeatils>()
-                .ForMember(dest => dest.Id, opt => opt.Ignore()) 
-                .ForMember(dest => dest.UserId, opt => opt.Ignore()) 
-                .ForMember(dest => dest.ToolId, opt => opt.Ignore()) 
-                .ForMember(dest => dest.ReservationId, opt => opt.Ignore()) 
-                .ForMember(dest => dest.Status, opt => opt.Ignore()) 
-                .ForMember(dest => dest.TotalPrice, opt => opt.Ignore()) 
-                .ForMember(dest => dest.LateFee, opt => opt.Ignore()) 
-                .ForMember(dest => dest.Date2Hire, opt => opt.Ignore()) 
-                .ForMember(dest => dest.Date2Return, opt => opt.Ignore()); 
+                .ForMember(dest => dest.Id, opt => opt.Ignore())
+                .ForMember(dest => dest.UserId, opt => opt.Ignore())
+                .ForMember(dest => dest.ReservationId, opt => opt.Ignore())
+                .ForMember(dest => dest.Status, opt => opt.Ignore())
+                .ForMember(dest => dest.TotalPrice, opt => opt.Ignore())
+                .ForMember(dest => dest.LateFee, opt => opt.Ignore())
+                .ForMember(dest => dest.CheckedOutAt, opt => opt.Ignore())
+                .ForMember(dest => dest.ReturnedAt, opt => opt.Ignore())
+                .ForMember(dest => dest.CreatedAt, opt => opt.Ignore())
+                .ForMember(dest => dest.UpdatedAt, opt => opt.Ignore()) // Set in service
+                .ForMember(dest => dest.User, opt => opt.Ignore())
+                .ForMember(dest => dest.Tool, opt => opt.Ignore())
+                .ForMember(dest => dest.Reservation, opt => opt.Ignore());
 
-            // RESPONSE: OrderDetails -> OrderSummaryDto 
+            // RESPONSE: OrderDeatils -> OrderSummaryDTO 
             CreateMap<OrderDeatils, OrderSummaryDTO>()
-                .ForMember(dest => dest.IsOverdue, opt => opt.Ignore()) 
-                .ForMember(dest => dest.ToolName, opt => opt.Ignore()) 
-                .ForMember(dest => dest.ToolBrand, opt => opt.Ignore()) 
-                .ForMember(dest => dest.ToolModel, opt => opt.Ignore()) 
-                .ForMember(dest => dest.UserName, opt => opt.Ignore()); 
+                .ForMember(dest => dest.IsOverdue, opt => opt.MapFrom(src =>
+                    src.Date2Return < DateTime.UtcNow && src.Status != "Returned"))
+                .ForMember(dest => dest.ToolName, opt => opt.MapFrom(src => src.Tool.Name))
+                .ForMember(dest => dest.ToolBrand, opt => opt.MapFrom(src => src.Tool.Brand))
+                .ForMember(dest => dest.ToolModel, opt => opt.MapFrom(src => src.Tool.Model))
+                .ForMember(dest => dest.UserName, opt => opt.MapFrom(src =>
+                    $"{src.User.FirstName} {src.User.LastName}"));
 
-            // RESPONSE: OrderDetails -> OrderDetailDto 
-            CreateMap<OrderDeatils, OrderSummaryDTO>()
-                           .ForMember(dest => dest.IsOverdue, opt => opt.Ignore()) 
-                           .ForMember(dest => dest.ToolName, opt => opt.Ignore()) 
-                           .ForMember(dest => dest.ToolBrand, opt => opt.Ignore()) 
-                           .ForMember(dest => dest.ToolModel, opt => opt.Ignore()) 
-                           .ForMember(dest => dest.UserName, opt => opt.Ignore()); 
-
-            // SUPPORTING: User -> OrderUserDto
+            // SUPPORTING: User -> OrderUserDTO
             CreateMap<User, OrderUserDTO>();
 
-            // SUPPORTING: Reservation -> OrderReservationDto
-            CreateMap<Reservation, OrderReservationDTO>()
-                .ForMember(dest => dest.RentalDays, opt => opt.MapFrom(src => src.Date2Hire))
-                .ForMember(dest => dest.IsOverdue, opt => opt.MapFrom(src => src.Date2Return));
+            // SUPPORTING: Tool -> OrderToolDTO
+            CreateMap<Tool, OrderToolDTO>();
 
+            // SUPPORTING: Reservation -> OrderReservationDTO
+            CreateMap<Reservation, OrderReservationDTO>()
+                .ForMember(dest => dest.RentalDays, opt => opt.MapFrom(src =>
+                    (src.Date2Return - src.Date2Hire).Days + 1))
+                .ForMember(dest => dest.IsOverdue, opt => opt.MapFrom(src =>
+                    src.Date2Return < DateTime.UtcNow && src.Status == "Active"))
+                .ForMember(dest => dest.DaysOverdue, opt => opt.MapFrom(src =>
+                    src.Date2Return < DateTime.UtcNow && src.Status == "Active"
+                        ? (DateTime.UtcNow - src.Date2Return).Days
+                        : 0))
+                .ForMember(dest => dest.CanBeCancelled, opt => opt.Ignore())
+                .ForMember(dest => dest.CanBeCheckedOut, opt => opt.Ignore())
+                .ForMember(dest => dest.CanBeReturned, opt => opt.Ignore());
         }
     }
 }
