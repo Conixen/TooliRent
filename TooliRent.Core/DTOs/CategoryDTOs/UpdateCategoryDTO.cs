d8a3437c48fe902d2a86571cae64a1f32af7c87c
Leon <leon.json57@gmail.com>
2025-10-21 20:03:24 +0200

    category

--- TooliRent.Core/DTOs/CategoryDTOs/UpdateCategoryDTO.cs
+++ TooliRent.Core/DTOs/CategoryDTOs/UpdateCategoryDTO.cs
@@ -2,17 +2,9 @@
 
 namespace TooliRent.DTO_s.CategoryDTOs
 {
-    public class ToolDTO  // Admin only
+    public class UpdateCategoryDTO  // Admin only
     {
-        public int Id { get; set; }
         public string Name { get; set; }
-        public string Brand { get; set; }
-        public string Model { get; set; }
         public string Description { get; set; }
-        public decimal PricePerDay { get; set; }
-        public bool IsAvailable { get; set; }
-        public string? SerialNumber { get; set; }
-        public int CategoryId { get; set; }
-        public string CategoryName { get; set; }
     }
 }
