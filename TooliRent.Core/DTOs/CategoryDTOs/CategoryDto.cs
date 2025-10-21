e420f1c3b726f0b377bd7d52affadbb979c8caa7
Leon <leon.json57@gmail.com>
2025-10-21 19:41:16 +0200

    category dto simpler

--- TooliRent.Core/DTOs/CategoryDTOs/CategoryDto.cs
+++ TooliRent.Core/DTOs/CategoryDTOs/CategoryDto.cs
@@ -1,9 +1,9 @@
 ﻿namespace TooliRent.DTO_s.CategoryDTOs
 {
-    public class CategoryDto    // admin snd user
+    public class CategoryDTO    // admin snd user
     {
         public int Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-        public string Description { get; set; } = string.Empty;
+        public string Name { get; set; }
+        public string Description { get; set; }
     }
 }
--- TooliRent.Core/DTOs/CategoryDTOs/CreateCategoryDTO.cs
+++ TooliRent.Core/DTOs/CategoryDTOs/CreateCategoryDTO.cs
@@ -4,14 +4,7 @@ namespace TooliRent.DTO_s.CategoryDTOs
 {
     public class CreateCategoryDTO  // Admin only
     {
-        [Required(ErrorMessage = "Category name is required")]
-        [MaxLength(50, ErrorMessage = "Name cannot exceed 50 characters")]
-        [MinLength(2, ErrorMessage = "Name must be at least 2 characters")]
-        public string Name { get; set; } = string.Empty;
-
-        [Required(ErrorMessage = "Description is required")]
-        [MaxLength(255, ErrorMessage = "Description cannot exceed 255 characters")]
-        [MinLength(10, ErrorMessage = "Description must be at least 10 characters")]
-        public string Description { get; set; } = string.Empty;
+        public string Name { get; set; }
+        public string Description { get; set; }
     }
 }
